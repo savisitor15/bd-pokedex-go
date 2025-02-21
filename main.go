7c6d426f50840248524d58f1d4359c9216b41de7
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2025-02-21 05:57:35 +0200

    Input cleanser created

--- main.go
+++ main.go
@@ -2,7 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func cleanInput(text string) []string {
+	splits := strings.Split(text, " ")
+	if len(splits) == 0 {
+		return []string{}
+	}
+	final := make([]string, 0, len(splits))
+	for _, spl := range splits {
+		if len(strings.TrimSpace(spl)) == 0 {
+			continue
+		}
+		final = append(final, strings.ToLower(strings.TrimSpace(spl)))
+	}
+	return final
+}
 
 func main() {
 	fmt.Println("Hello, World!")
---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("Miss-matched lengths! expected: %d actual: %d", len(c.expected), len(actual))
+			return
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("Word %s and expected word %s, don't match", word, expectedWord)
+				return
+			}
+		}
+	}
+		
+}
+
